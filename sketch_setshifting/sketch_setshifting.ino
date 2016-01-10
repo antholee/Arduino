0e93b8fd7587d70ae4089c24471d467009edcbae
antholee_MBP <antholee@gmail.com>
2016-01-10 01:20:59 -0800

    working prototype v1
    working prototype with lights, sounds, toy motor, IR beam break, IR
    distance sensor. next step includes operating CD drive and writing
    actual protocol code instead of running things through serial monitor

--- sketch_setshifting/sketch_setshifting.ino
+++ sketch_setshifting/sketch_setshifting.ino
@@ -1,13 +1,19 @@
 
 const int lLEDpin = 2;
 const int rLEDpin = 3;
-const int lSpeaker = 4;
-const int rSpeaker = 5;
-const int door = 6;
+const int lSpeakerPin = 4;
+const int rSpeakerPin = 5;
+const int LEDpin = 6; //replace with "door"
+const int lSensorPin = 7; 
+const int rSensorPin = 8;
+const int motorPin = 9;
 const int time_stimDelays = 500;
 const int sensorWallIR = A0;
 const long ledInterval = 250;           // in milliseconds
 const int stimDuration = 2000; //in milliseconds
+const int motorDuration = 5000; //in milliseconds
+ 
+int stimArray[4] = {1, 2, 3, 4};
 
 int wallIRval = 0; //over 600au is closest distance to sensor
 /* stimulus 0: turn off all stimulus
@@ -15,8 +21,9 @@ stimulus 1: left LED + left speaker
 stimulus 2: left LED + right speaker
 stimulus 3: right LED + left speaker
 stimulus 4: right LED + right speaker */
+int ledState = LOW;             // ledState used to set the door LED
+int lSensorState = 0, last_lSensorState = 0, rSensorState = 0, last_rSensorState = 0;
 
-int ledState = LOW;             // ledState used to set the LED
 
 
 
@@ -24,9 +31,14 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(lLEDpin, OUTPUT); //left LED
   pinMode(rLEDpin, OUTPUT); //right LED
-  pinMode(lSpeaker, OUTPUT); //left speaker
-  pinMode(rSpeaker, OUTPUT); //right speaker
-  pinMode(door, OUTPUT); 
+  pinMode(lSpeakerPin, OUTPUT); //left speaker
+  pinMode(rSpeakerPin, OUTPUT); //right speaker
+  pinMode(LEDpin, OUTPUT); 
+  pinMode(lSensorPin, INPUT); //IR in left nose poke
+  pinMode(rSensorPin, INPUT); //IR in right nose poke
+  pinMode(motorPin, OUTPUT);
+  digitalWrite(lSensorPin, HIGH); // turn on the pullup
+  digitalWrite(rSensorPin, HIGH); // turn on the pullup
   Serial.begin(9600);
   Serial.println("Enter stimulus # 1 to 4 or '0' for no stimulus");
   
@@ -46,34 +58,47 @@ void onLED(int ledPin) {
     digitalWrite(ledPin, LOW);
 }
 
+void onMotor(int motorPin, int speed){
+  unsigned long prevMillis = millis();
+  unsigned long stimMillis = prevMillis + motorDuration;
+
+  while (prevMillis<stimMillis){
+    analogWrite(motorPin, speed);
+    unsigned long currentMillis = millis();
+    prevMillis = currentMillis;
+    }
+  
+    analogWrite(motorPin, 0);
+}
+
 
 void playStimulus(int stim) {
 
   if (stim == 0) {
     digitalWrite(2, LOW);
     digitalWrite(3, LOW);
-    noTone(lSpeaker);
-    noTone(rSpeaker);
+    noTone(lSpeakerPin);
+    noTone(rSpeakerPin);
   }
   
   if (stim == 1) {
-    tone(lSpeaker, 3000, stimDuration);
+    tone(lSpeakerPin, 3000, stimDuration);
     onLED(lLEDpin);
     }
     
   
   if (stim == 2) {  
-    tone(rSpeaker, 3000, stimDuration);  
+    tone(rSpeakerPin, 3000, stimDuration);  
     onLED(lLEDpin);
   }
 
   if (stim == 3) {   
-    tone(lSpeaker, 3000, stimDuration);
+    tone(lSpeakerPin, 3000, stimDuration);
     onLED(rLEDpin); 
   }
   
    if (stim == 4) {
-    tone(rSpeaker, 3000, stimDuration);
+    tone(rSpeakerPin, 3000, stimDuration);
      onLED(rLEDpin); 
    }
 }
@@ -81,14 +106,53 @@ void playStimulus(int stim) {
  
 void loop() {
   // put your main code here, to run repeatedly:
+asdf
+
+
+// code for left and right poke sensors
+lSensorState = digitalRead(lSensorPin);
+  if (lSensorState == LOW) {     
+    // turn LED on:
+    digitalWrite(LEDpin, HIGH);  
+  } 
+  else {
+    // turn LED off:
+    digitalWrite(LEDpin, LOW); 
+  }
+  
+  if (lSensorState && !last_lSensorState) {
+    Serial.println("left sensor UNBROKEN");
+  } 
+  if (!lSensorState && last_lSensorState) {
+    Serial.println("left sensor BROKEN");
+  }
+  last_lSensorState = lSensorState;
+
+
+rSensorState = digitalRead(rSensorPin);
+  if (rSensorState == LOW) {     
+    // turn LED on:
+    digitalWrite(LEDpin, HIGH);  
+  } 
+  else {
+    // turn LED off:
+    digitalWrite(LEDpin, LOW); 
+  }
+  
+  if (rSensorState && !last_rSensorState) {
+    Serial.println("right sensor UNBROKEN");
+  } 
+  if (!rSensorState && last_rSensorState) {
+    Serial.println("right sensor BROKEN");
+  }
+  last_rSensorState = rSensorState;
+  
 
 
  if (Serial.available())
   {
-  
     char ch = Serial.read();
     int tempStim = ch - '0';
-    
     String dispStim = "Current stimulus is: ";
     String curStim = dispStim + tempStim;
     Serial.println(curStim);
@@ -101,12 +165,14 @@ void loop() {
 
   wallIRval = analogRead(sensorWallIR);
   if (wallIRval > 600){
-    digitalWrite(door, HIGH);
+    digitalWrite(LEDpin, HIGH);
+    onMotor(motorPin, 200);
   }
-  else digitalWrite(door, LOW);
+  else digitalWrite(LEDpin, LOW);
 
-    
- 
+
+
+  
  
 
 }
